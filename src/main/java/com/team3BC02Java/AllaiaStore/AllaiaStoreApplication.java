06e0f387a5d69f337ffe8cb779691c398739f137
hatrq <hatruongkhanh98@gmail.com>
2023-10-15 22:40:59 +0700

    new project

---
+++ src/main/java/com/team3BC02Java/AllaiaStore/AllaiaStoreApplication.java
@@ -0,0 +1,13 @@
+package com.team3BC02Java.AllaiaStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllaiaStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllaiaStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/team3BC02Java/AllaiaStore/AllaiaStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team3BC02Java.AllaiaStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllaiaStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
